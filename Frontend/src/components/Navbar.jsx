e8283f054da3dd4052534b6ee96e88b478c2a2b1
GuyHasan <guy28901@gmail.com>
2025-06-03 16:50:18 +0300

    Refactor Navbar component; implement authentication state handling and adjust layout for better responsiveness

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+	const { isAuthenticated } = useSelector((state) => state.user);
+	const handleLogout = () => {};
 	return (
 		<>
 			<header className='sticky-top fw-bold' style={{ backgroundColor: "#fcfcfc", boxShadow: "0 0 2px rgba(0, 0, 0, 0.1)" }}>
-				<nav className='d-flex align-items-center  px-5 py-3 gap-5 w-100'>
-					<div className='d-flex align-items-center gap-3 mx-auto'>
+				<nav className='d-grid align-items-center gap-5  px-5 py-3 w-100' style={{ gridTemplateColumns: "1fr auto 1fr" }}>
+					<div></div>
+					<div className='d-flex align-items-center gap-3'>
 						<NavLink className='nav-link' to='/'>
 							תפריט
 						</NavLink>
@@ -22,13 +26,24 @@ function Navbar() {
 							כשרות
 						</NavLink>
 					</div>
-					<div className='d-flex align-items-center gap-3'>
-						<NavLink className='nav-link' to='/login'>
-							התחברות
-						</NavLink>
-						<NavLink className='nav-link' to='/register'>
-							הרשמה
-						</NavLink>
+					<div className='d-flex align-items-center justify-content-end gap-3'>
+						{isAuthenticated ? (
+							<>
+								<NavLink className='nav-link' to='/menu'>
+									הפרופיל שלי
+								</NavLink>
+								<button onClick={handleLogout}>התנתקות</button>
+							</>
+						) : (
+							<>
+								<NavLink className='nav-link' to='/login'>
+									התחברות
+								</NavLink>
+								<NavLink className='nav-link' to='/register'>
+									הרשמה
+								</NavLink>
+							</>
+						)}
 					</div>
 				</nav>
 			</header>
