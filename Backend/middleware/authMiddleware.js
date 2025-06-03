417416121eaf2298d18c0b8ceaa3fd05193cbe37
GuyHasan <guy28901@gmail.com>
2025-06-03 18:03:23 +0300

    Enhance error handling in authMiddleware; include error type in authentication error for improved clarity

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -17,6 +17,6 @@ export default function authMiddleware(req, res, next) {
 		req.user = decoded;
 		next();
 	} catch (err) {
-		return next(new CustomError("Invalid or expired token", 401));
+		return next(new CustomError("Invalid or expired token", 401, "Authentication Error"));
 	}
 }
