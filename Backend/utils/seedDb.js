53b5bdadd2189696ecbda4d9e8f9cf544d25c20f
GuyHasan <guy28901@gmail.com>
2025-06-04 18:35:30 +0300

    feat(seedDb): add example product data and implement product seeding logic

--- Backend/utils/seedDb.js
+++ Backend/utils/seedDb.js
@@ -1,9 +1,13 @@
 import User from "../users/models/mongoDB/userSchema.js";
+import Product from "../products/models/mongoDB/productSchema.js";
 import { hashPassword } from "../users/services/passwordsServices.js";
+import { exampleData } from "../config/constants/exampleData.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const adminPassword = process.env.ADMIN_PASSWORD || "admin1234567";
+const useExampleData = process.env.USE_EXAMPLE_DATA;
 const seedDB = async () => {
 	const userCount = await User.countDocuments();
 	if (userCount > 0) {
@@ -22,6 +26,28 @@ const seedDB = async () => {
 
 	await adminUser.save();
 	console.log("✅ Admin user seeded successfully");
+
+	const productCount = await Product.countDocuments();
+	if (productCount > 0) {
+		console.log("Products already seeded. Skipping...");
+		return;
+	}
+	if (!useExampleData) {
+		console.log("chosen not to use example data, skipping product seeding");
+		return;
+	}
+	if (useExampleData) {
+		const products = exampleData.map((product) => ({
+			...product,
+			size: product.size.map((size) => ({
+				quantity: size.quantity,
+				price: size.price,
+			})),
+		}));
+
+		await Product.insertMany(products);
+		console.log("✅ Example products seeded successfully");
+	}
 };
 
 export default seedDB;
